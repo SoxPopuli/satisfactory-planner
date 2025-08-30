161e97fc621c1ad91df1164fe0d055c8ef79979b
Charlotte Smith <charliesmith5019@gmail.com>
2025-08-30 23:13:22 +0100

    initial commit

---
+++ tmux-start-dev.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Top Right
+tmux splitw -dh "npm run res:dev"
+
+# Top Left
+tmux splitw -dbv "npm run dev"
+
+tmux selectp -R
+
+# Bottom Right
+# tmux splitw -dv "$SHELL"
+
+tmux selectp -l
