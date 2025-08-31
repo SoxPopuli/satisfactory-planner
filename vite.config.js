8db03854ecb65d3a9da91c2940cb7b3a37042f5b
Charlotte Smith <47333659+SoxPopuli@users.noreply.github.com>
2025-08-31 01:33:14 +0100

    vendor react chunk (#2)
    * test
    
    * vendor react chunk

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,17 @@ export default defineConfig({
       include: ["**/*.res.mjs"],
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("react") || id.includes("react-dom")) {
+            return "react-vendor";
+          } else {
+            return undefined;
+          }
+        },
+      },
+    },
+  },
 });
